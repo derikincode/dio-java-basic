e6ed5099ea1285ee2238e89523f5728b1c6e412c
derikincode <derikoliveira252@gmail.com>
2024-04-26 23:30:54 -0300

    commit java Anatomia das Classes - 02

---
+++ trila-java-básico/java-anatomia-classes-02/java-anatomia-classes-02/src/MinhaClasse.java
@@ -0,0 +1,11 @@
+public class MinhaClasse {
+    public static void main (String [] args) {
+
+        int ano = 2021;
+
+        ano = 2022;
+
+        /* váriavel escrtia em maiuscula é uma constante*/
+        final String BR = "Brasil"; 
+    } 
+}
