576af70c4c6139397d5e51abfedd4e89ec82232b
derikincode <derikoliveira252@gmail.com>
2024-05-01 02:04:11 -0300

    Aula concluída

---
+++ aprendendo-a-sintaxe-java/java-operadores/src/Operadores03.java
@@ -0,0 +1,12 @@
+public class Operadores03 {
+    public static void main(String [] args) {
+        int numero = 5;
+
+        numero = - numero;
+        System.out.println(numero);
+
+        numero = numero * -1;
+        //System.out.println(- numero);
+        System.out.println(numero);
+    }
+}
---
+++ aprendendo-a-sintaxe-java/java-operadores/src/Operadores04.java
@@ -0,0 +1,26 @@
+public class Operadores04 {
+    public static void main(String[] args) {
+        int numero = 5;
+        //x repetição
+        //numero++; //numero = numero + 1;
+
+    
+        System.out.println(++numero);
+
+
+        System.out.println(numero --);
+        System.out.println(numero);
+
+
+        //
+
+        boolean variavel = true;
+        //System.out.println(!variavel);
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+    }
+
+}
