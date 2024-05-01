b9cc49642d077f50d0d457dff679b785005e2907
derikincode <derikoliveira252@gmail.com>
2024-04-30 21:11:46 -0300

    Aula concluída

--- aprendendo-a-sintaxe-java/java-tipos-e-variaveis/src/TiposVariaveis.java
+++ aprendendo-a-sintaxe-java/java-tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Tipos primitivos
+        //Estudar a classe String que representa textos na aplicação
+
+        String meuNome = "DERIK OLIVEIRA";
+        
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 38484848;
+        long cepf = 992929292L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
